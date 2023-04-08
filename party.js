3aaf3b7f1e2e2d29d94f80f8345b8db16ff9f3a0
Garret Mitchell <garretmitchell@Garrets-MacBook-Pro.local>
2023-04-08 15:49:06 -0400

    complete

---
+++ party.js
@@ -0,0 +1,41 @@
+const $gifArea = $("#gif-area");
+const $searchInput = $("#search");
+
+/* use ajax to append gif */
+
+function addGif(res) {
+  let numResults = res.data.length;
+  if (numResults) {
+    let randomIdx = Math.floor(Math.random() * numResults);
+    let $newCol = $("<div>", { class: "col-md-4 col-12 mb-4" });
+    let $newGif = $("<img>", {
+      src: res.data[randomIdx].images.original.url,
+      class: "w-100"
+    });
+    $newCol.append($newGif);
+    $gifArea.append($newCol);
+  }
+}
+
+/* handle form submission */
+
+$("form").on("submit", async function(evt) {
+  evt.preventDefault();
+
+  let searchTerm = $searchInput.val();
+  $searchInput.val("");
+
+  const response = await axios.get("http://api.giphy.com/v1/gifs/search", {
+    params: {
+      q: searchTerm,
+      api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"
+    }
+  });
+  addGif(response.data);
+});
+
+/* remove images */
+
+$("#remove").on("click", function() {
+  $gifArea.empty();
+});
